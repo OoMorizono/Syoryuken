1db6358f73b7e6716a3873ebaaf6119872cddcd2
OoMorizono <x65f588d72sw76@gmail.com>
2021-11-01 19:45:28 +0900

    Initial Commit

---
+++ Syoryuken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Syoryuken
+//
+//  Created by 森園王 on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
